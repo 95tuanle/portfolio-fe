393bc1a4a9e3b7b1e8988bb281c826d44ca3156e
Tuan Le <tlna95@icloud.com>
2023-08-03 00:59:38 -0400

    clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
-  return (
-    <>
-        <h1>Portfolio Front-end</h1>
-        <p>Nguyen Anh Tuan Le</p>
-    </>
-  )
+    return (
+        <>
+            <h1>Portfolio Front-end</h1>
+            <p>Nguyen Anh Tuan Le</p>
+        </>
+    )
 }
