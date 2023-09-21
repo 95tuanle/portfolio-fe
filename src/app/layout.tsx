614bdecd90dca217db66e508c1f702442d818761
Tuan Le <tlna95@icloud.com>
2023-09-21 00:08:08 -0400

    forgot to init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,16 @@
 import './globals.css'
 import {Inter} from 'next/font/google'
 import React from "react";
+import {Analytics} from '@vercel/analytics/react';
 
 const inter = Inter({subsets: ['latin']})
 
 const Layout = ({children}: { children: React.ReactNode }) => (
     <html lang="en">
-    <body className={inter.className}>{children}</body>
+    <body className={inter.className}>
+    {children}
+    <Analytics/>
+    </body>
     </html>
 );
 
