188464532913a3fac1778db36a72887fbbdcee8b
95tuanle <tlna95@icloud.com>
2023-10-18 03:00:33 -0400

    upgrade packages and change spacings

--- src/components/github-user-repos.tsx
+++ src/components/github-user-repos.tsx
@@ -1,12 +1,12 @@
 import GitHubUserRepo from "@/components/github-user-repo";
 
 const GitHubUserRepos = ({gitHubUserRepos}: any) => {
-    return (
-        <>
-            {gitHubUserRepos.map((gitHubUserRepo: any) => (
-                <GitHubUserRepo gitHubUserRepo={gitHubUserRepo} key={gitHubUserRepo["id"]}/>))}
-        </>
-    );
+  return (
+    <>
+      {gitHubUserRepos.map((gitHubUserRepo: any) => (
+        <GitHubUserRepo gitHubUserRepo={gitHubUserRepo} key={gitHubUserRepo["id"]}/>))}
+    </>
+  );
 };
 
 export default GitHubUserRepos;
