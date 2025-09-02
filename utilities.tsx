d789f0746818450ec45ad2413bb90fa5594dfd93
Nguyen Anh Tuan Le <tlna95@icloud.com>
2025-09-03 01:21:27 +0700

    Update utilities.tsx

--- utilities.tsx
+++ utilities.tsx
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "https://portfolio-be.adaptable.app";
+export const BACKEND_URL = "https://portfolio-be-7l4j.onrender.com";
 
 export const fetchApi = async (api: string) => {
   const response = await fetch(api);
